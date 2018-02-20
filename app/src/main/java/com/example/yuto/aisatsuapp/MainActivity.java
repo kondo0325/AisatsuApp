336ccc060c0b59bf8c20ebbd458b0bafcd5a0388
yuto <kondo0325@gmail.com>
2018-02-21 02:39:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/yuto/aisatsuapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.yuto.aisatsuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+
+import android.widget.Button;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        showTimePickerDialog();
+    }
+
+
+
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        if(hourOfDay >= 2 && hourOfDay <= 9) mTextView.setText("おはよう");
+                        else if(hourOfDay >= 10 && hourOfDay <= 17) mTextView.setText("こんにちは");
+                        else mTextView.setText("こんばんは");
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+
+
+}
